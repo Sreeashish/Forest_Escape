c8b3c476ec6084d241a70a951fdb653856753db5
Sreeashish <sreeashish2001@gmail.com>
2024-03-08 09:37:03 -0500

    Input UI changes

--- Assets/Scripts/UiController.cs
+++ Assets/Scripts/UiController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class UiController : MonoBehaviour
 {
     public CanvasGroup HUDCanvas, cinematicCanvas, deathScreen, blackScreen;
-    public Image lifeBar, CinematicTopBar, CinematicBottomBar;
+    public Image lifeBar, CinematicTopBar, CinematicBottomBar, heartImage;
     public RectTransform respawnTextHolder;
 
     public static UiController instance;
@@ -16,10 +16,16 @@ public class UiController : MonoBehaviour
             instance = this;
     }
 
+    Coroutine HeartBeatRoutine;
     private void Start()
     {
         CalculateLife();
         RemoveDeathScreen();
+        if(HeartBeatRoutine != null)
+        {
+            StopCoroutine(HeartBeatRoutine);
+        }
+        HeartBeatRoutine = StartCoroutine(HeartBeat());
     }
 
     public IEnumerator BringCinematicFramesIn()
@@ -52,6 +58,19 @@ public class UiController : MonoBehaviour
         respawnTextHolder.DOScaleY(0, 0);
         respawnTextHolder.DOScaleY(1.5f, 1.5f).SetEase(Ease.Linear);
 
+    }
+
+    IEnumerator HeartBeat()
+    {
+        while (true)
+        {
+            yield return null;
+            heartImage.transform.DOScale(1, 0);
+            heartImage.transform.DOScale(0.8f, 0.5f);
+            yield return CommonScript.GetDelay(0.8f);
+            heartImage.transform.DOScale(1, 0.5f);
+            yield return CommonScript.GetDelay(0.8f);
+        }
     }
 
     public void RemoveDeathScreen()
